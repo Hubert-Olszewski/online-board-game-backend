cafdcb6e775f6fc4a89fd6f8621c881a5647de71
Hubert Olszewski <holszewski@consorg.pl>
2022-08-20 20:41:12 +0200

    Comunication with clients

--- index.js
+++ index.js
@@ -8,17 +8,96 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+const serverPort = 3001;
+const domainClientURL = "http://localhost:" + (serverPort - 1);
+const domainServerUrl = "http://localhost:" + serverPort;
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: domainClientURL,
     methods: ["GET", "POST"],
   },
 });
 
+const gamesInSession = [];
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-  console.log(socket.nsp.sockets.size);
-  
+  gamesInSession.push(socket);
+
+  socket.on("disconnect", () => onDisconnect(socket));
+  socket.on("createNewGame", (gameId) => createNewGame(socket, gameId));
+  socket.on("playerJoinGame", (data) => playerJoinsGame(socket, data));
+  socket.on('requestUsername', (gameId) => requestUserName(socket, gameId));
+  socket.on('recievedUserName', (data) => recievedUserName(socket, data));
+  // socket.on("newMove", newMove);
+});
+
+const playerJoinsGame = (socket, idData) => {
+  console.log('playerJoinsGame idData: ', idData);
+  console.log('rooms: ', io.sockets.adapter.rooms);
+  const room = io.sockets.adapter.rooms.get(idData.gameId);
+  console.log('room: ', room);
+
+  if (room === undefined) {
+    socket.to(socket.id).emit('status' , "This game session does not exist." );
+    return;
+  }
+
+  if (room.size < 2) {
+    idData.mySocketId = socket.id;
+
+    socket.join(idData.gameId);
+
+    console.log('room.size', room.size); 
+
+    if (room.size === 2) {
+      io.sockets.in(idData.gameId).emit('startGame', idData.userName);
+    }
+
+    io.sockets.in(idData.gameId).emit('playerJoinedRoom', idData);
+
+  } else {
+    console.log();
+    socket.to(socket.id).emit('status' , "There are already 2 people playing in this room." );
+  }
+}
+
+const createNewGame = (socket, gameId) => {
+  console.log('CreateNewGame - server');
+  socket.join(gameId);
+  socket.emit('createNewGame', {gameId: gameId, mySocketId: socket.id});  
+}
+
+const onDisconnect = (socket) => {
+  console.log('onDisconnect');
+  const index = gamesInSession.indexOf(socket);
+  gamesInSession.splice(index, 1);
+}
+
+const requestUserName = (socket, gameId) => {
+  console.log('requestUserName: ' + socket + ' gameID: ' + gameId);
+  io.to(gameId).emit('giveUserName', socket.id);
+}
+
+const recievedUserName = (socket, data) => {
+  console.log('recievedUserName');
+  data.socketId = socket.id
+  io.to(data.gameId).emit('getOpponentUserName', data);
+}
+
+const newMove = (move) => {
+  io.to(move.gameId).emit('opponentMove', move);
+}
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
+
+
+
+
+
+const chatInit = (socket) => {
   socket.emit("joinToRoom", "room");
 
   socket.on("joinRoom", (data) => {
@@ -38,9 +117,4 @@ io.on("connection", (socket) => {
       }
     }
   });
-
-});
- 
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
-});
+}
